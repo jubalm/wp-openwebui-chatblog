e7a3dc527dee49b176e1c30397dadf5aa9fb0bbf
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2025-07-01 11:03:22 +0800

    fix: use remote state for kubeconfig

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -59,3 +59,46 @@ output "datacenter_id" {
 output "lan_id" {
   value = ionoscloud_lan.db_lan.id
 }
+
+variable "cluster_name" {
+  type    = string
+  default = "mks-cluster"
+}
+
+variable "k8s_version" {
+  type    = string
+  default = "1.32.5"
+}
+
+resource "ionoscloud_k8s_cluster" "mks" {
+  name        = var.cluster_name
+  k8s_version = var.k8s_version
+  maintenance_window {
+    day_of_the_week = "Monday"
+    time            = "03:00:00Z"
+  }
+}
+
+resource "ionoscloud_k8s_node_pool" "mks_pool" {
+  k8s_cluster_id    = ionoscloud_k8s_cluster.mks.id
+  name              = "mks-node-pool"
+  node_count        = 2
+  cpu_family        = "INTEL_SIERRAFOREST"
+  ram_size          = 4096
+  availability_zone = "AUTO"
+  datacenter_id     = ionoscloud_datacenter.main.id
+  k8s_version       = var.k8s_version
+  cores_count       = 2
+  storage_type      = "SSD"
+  storage_size      = 20
+
+  lans {
+    id   = ionoscloud_lan.db_lan.id
+    dhcp = true
+  }
+}
+
+output "kubeconfig" {
+  value     = ionoscloud_k8s_cluster.mks.kube_config
+  sensitive = true
+}
--- terraform/platform/main.tf
+++ terraform/platform/main.tf
@@ -35,19 +35,32 @@ terraform {
   }
 }
 
+data "terraform_remote_state" "infra" {
+  backend = "s3"
+  config = {
+    bucket = "demo-vdc-backend-store"
+    key    = "infrastructure/terraform.tfstate"
+    region = "eu-central-2"
+    endpoints = {
+      s3 = "https://s3-eu-central-2.ionoscloud.com"
+    }
+    use_path_style              = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
+  }
+}
+
 provider "kubernetes" {
-  config_path = var.kubeconfig_path
+  kubeconfig = data.terraform_remote_state.infra.outputs.kubeconfig
 }
 
 provider "helm" {
   kubernetes = {
-    config_path = var.kubeconfig_path
+    kubeconfig = data.terraform_remote_state.infra.outputs.kubeconfig
   }
-}
-
-variable "kubeconfig_path" {
-  type    = string
-  default = "~/.kube/config"
 }
 
 provider "ionoscloud" {
@@ -58,47 +71,7 @@ variable "ionos_token" {
   type = string
 }
 
-variable "cluster_name" {
-  type    = string
-  default = "mks-cluster"
-}
 
-variable "k8s_version" {
-  type    = string
-  default = "1.32.5"
-}
-
-resource "ionoscloud_k8s_cluster" "mks" {
-  name        = var.cluster_name
-  k8s_version = var.k8s_version
-  maintenance_window {
-    day_of_the_week = "Monday"
-    time            = "03:00:00Z"
-  }
-}
-
-data "ionoscloud_k8s_cluster" "mks" {
-  name = ionoscloud_k8s_cluster.mks.name
-}
-
-resource "ionoscloud_k8s_node_pool" "mks_pool" {
-  k8s_cluster_id    = ionoscloud_k8s_cluster.mks.id
-  name              = "mks-node-pool"
-  node_count        = 2
-  cpu_family        = "INTEL_SIERRAFOREST"
-  ram_size          = 4096
-  availability_zone = "AUTO"
-  datacenter_id     = data.terraform_remote_state.infra.outputs.datacenter_id
-  k8s_version       = var.k8s_version
-  cores_count       = 2
-  storage_type      = "SSD"
-  storage_size      = 20
-
-  lans {
-    id   = data.terraform_remote_state.infra.outputs.lan_id
-    dhcp = true
-  }
-}
 
 resource "ionoscloud_pg_cluster" "postgres" {
   display_name         = "postgres-cluster"
@@ -244,9 +217,7 @@ resource "ionoscloud_pg_database" "authentik" {
   owner      = "authentikuser"
 }
 
-output "cluster_id" {
-  value = ionoscloud_k8s_cluster.mks.id
-}
+
 
 output "kubeconfig" {
   value     = data.ionoscloud_k8s_cluster.mks.kube_config
