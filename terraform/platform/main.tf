2d6ff7bbdb10b16d0495245430a0e337b1e21de9
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2025-07-01 11:20:16 +0800

    fix: remove duplicate remote state data source

--- terraform/platform/main.tf
+++ terraform/platform/main.tf
@@ -35,23 +35,7 @@ terraform {
   }
 }
 
-data "terraform_remote_state" "infra" {
-  backend = "s3"
-  config = {
-    bucket = "demo-vdc-backend-store"
-    key    = "infrastructure/terraform.tfstate"
-    region = "eu-central-2"
-    endpoints = {
-      s3 = "https://s3-eu-central-2.ionoscloud.com"
-    }
-    use_path_style              = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    skip_region_validation      = true
-    skip_s3_checksum            = true
-  }
-}
+
 
 provider "kubernetes" {
   kubeconfig = data.terraform_remote_state.infra.outputs.kubeconfig
